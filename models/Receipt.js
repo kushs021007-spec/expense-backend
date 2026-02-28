d1225b576432fce7bbb268f0813b42eb08702d32
vidhi0212-cloud <ranavidhi1234567@gmail.com>
2026-02-28 23:22:54 +0530

    Add Receipt model with schema definition

--- models/Receipt.js
+++ models/Receipt.js
@@ -1,1 +1,21 @@
+import mongoose from "mongoose";
 
+const receiptSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  amount: Number,
+  description: String,
+  date: String,
+  status: {
+    type: String,
+    enum: [
+      "draft",
+      "submitted",
+      "manager_approved",
+      "approved",
+      "rejected"
+    ],
+    default: "submitted"
+  }
+}, { timestamps: true });
+
+export default mongoose.model("Receipt", receiptSchema);
