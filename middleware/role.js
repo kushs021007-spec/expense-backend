0a12ba8137716afe9deabdcd1bceebab5f9cf860
vidhi0212-cloud <ranavidhi1234567@gmail.com>
2026-02-28 23:24:25 +0530

    Add role middleware for access control

--- middleware/role.js
+++ middleware/role.js
@@ -1,1 +1,10 @@
+const role = (allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access Denied" });
+    }
+    next();
+  };
+};
 
+export default role;
