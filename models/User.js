93f85a4cc55ab60c529815ec6860dca035aca718
vidhi0212-cloud <ranavidhi1234567@gmail.com>
2026-02-28 23:22:23 +0530

    Add User model with schema and timestamps

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: {
+    type: String,
+    enum: ["employee", "manager", "cfo"]
+  },
+  managerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
