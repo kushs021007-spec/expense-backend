f20fe321b29e8f05fb6f597f6d0e9dedda8416d1
kushs021007-spec <kushs021007@gmail.com>
2026-03-01 16:25:34 +0530

    Refactor server.js for improved connection and logging

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
-app.use(cors());
+connectDB();
+
+app.use(cors({
+  origin: "https://kushs021007-spec.github.io"
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log("Server Running on Port", PORT)
-);
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
